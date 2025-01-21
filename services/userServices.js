657250a67bb1323bb4ad5822baad2083a98de54f
Yamlak1 <yamifikru4@gmail.com>
2025-01-21 17:51:05 +0300

    push

--- services/userServices.js
+++ services/userServices.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 
-const BACKEND_URL = "http://localhost:8080/api/users"; // Backend API URL
+// const BACKEND_URL = "https://bounce-backend-ubdt.onrender.com";
+const BACKEND_URL = "http://localhost:8080/api/users";
 
 // Function to register a user
 const registerUser = async (chatId, username) => {
@@ -19,11 +20,10 @@ const registerUser = async (chatId, username) => {
   }
 };
 
-// Function to check if a user exists
 const checkUser = async (chatId) => {
   try {
     const response = await axios.post(`${BACKEND_URL}/check`, { chatId });
-    return response.data.exists || !!response.data.id; // User exists if id is present or 'exists' is true
+    return response.data.exists || !!response.data.id;
   } catch (error) {
     if (error instanceof Error) {
       console.error("Error checking user:", error.message);
