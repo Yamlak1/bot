20bd7efb29980574c06f2d91386f65dc5f3ffe23
Yamlak1 <yamifikru4@gmail.com>
2025-01-21 18:19:03 +0300

    a

--- services/userServices.js
+++ services/userServices.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const BACKEND_URL = "https://bounce-backend-ubdt.onrender.com";
+const BACKEND_URL = "https://bounce-backend-ubdt.onrender.com/api/users";
 // const BACKEND_URL = "http://localhost:8080/api/users";
 
 // Function to register a user
